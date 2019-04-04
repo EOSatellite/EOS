e867ad236c263738b8e53339029988b8b18cad0c
Joe Sitton <sittonjoe@gmail.com>
2019-04-03 21:33:23 -0400

    update math, but now idk

--- Python/graph_fg.py
+++ Python/graph_fg.py
@@ -6,7 +6,7 @@ with open("fg.txt", "r", encoding="ISO-8859-1") as f:
 data = [d.strip() for d in data]
 data = [int(d) for d in data if d in ["0", "1"]]
 
-lil_d = data[:500]
+lil_d = data[:]
 
 vals = []
 while 1:
@@ -33,10 +33,10 @@ i = 0
 for v in range(0, len(vals), 2):
     try:
         T = len(vals[v]) + len(vals[v+1])
-        print(f"T{i} = ", T, "\trpm = ", T * 2 * 60)
+        freq = 1/T
+        rpm = (freq * 120) / 4
+
+        print(f"T{i} = ", T, "\trpm = ", rpm)
     except:
         continue
     i += 1
-
-plt.plot(data[:500])
-plt.show()
